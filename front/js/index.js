99a02ef55c954868d08bac2255f0f09ff7880971
mohamed khounti <30549055+larbikhounti@users.noreply.github.com>
2023-04-18 01:50:07 +0000

    making sure userid us there

--- front/js/index.js
+++ front/js/index.js
@@ -5,8 +5,11 @@ import  {setCookies} from './cookies/cookiesHandler.js'
 function setTrackingUserId(userId) {
     // Set a cookie with name `trackingUserId` and value `userId`, to be expired never and to be valid for the entire site
     setCookies("trackingUserId",userId)
-    // Call the `startListnening` function to start listening for events
-    startListnening()
+    if (userId.length> 0 && userId !== null) {
+        // Call the `startListnening` function to start listening for events
+        startListnening()
+    }
+   
 }
 
 // Export the `setTrackingUserId` function
